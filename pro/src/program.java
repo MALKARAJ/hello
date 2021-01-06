e65d738bfc65a9bb8944687d0e65dc3a31f0966e
MALKARAJ <malkaraj99@gmail.com>
2021-01-06 14:59:31 +0530

    second program fifth version with 2 if and 2 else statement

--- pro/src/program.java
+++ pro/src/program.java
@@ -4,7 +4,15 @@ public class program {
     public static void main(String[] args) {
 
 
-      
+        int weight=0;
+        if(weight >=0 && weight <=25)
+            System.out.println("under weight");
+        else
+            if(weight >25 && weight<=50)
+                System.out.println("proper weight");
+
+            else
+                System.out.println("over weight");
     }
 }
 
